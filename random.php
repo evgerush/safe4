539904547d274a3d9507b69c478398c8bc5bf01e
eviljay <evgencherkashyn@gmail.com>
2022-02-27 00:24:02 +0200

    mmm

---
+++ random.php
@@ -0,0 +1,18 @@
+<?
+$num=rand(1, 5) ;
+if ($num=1){
+	header("Location: http://safeua.online");
+}
+elseif ($num=2){
+	header("Location: https://safeua.cyou");
+}
+elseif ($num=3){
+	header("Location: https://safeua.rest");
+}
+elseif ($num=4){
+	header("Location: https://safeua.world");
+}
+elseif ($num=5){
+	header("Location: https://safeua.top");
+}
+?>
